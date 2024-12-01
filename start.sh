237c47579c69dd7ee60f88be9447faab5c18d027
Gabe Ulrich Laptop <gabriel.ulrich@aol.com>
2024-12-01 14:27:14 -0500

    shell script

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-# Run npm start
-echo "Running 'npm run start'..."
-npm run start || {
-    echo "Failed to start the application."
-    exit 1
-}
+cd /algo
+git pull
+npm run start
+echo finished!
