8b4e4f81f508146baf786e58105f353a632051d4
Gabe Ulrich Laptop <gabriel.ulrich@aol.com>
2024-12-05 11:01:56 -0500

    start shell update

--- start.sh
+++ start.sh
@@ -1,34 +1,74 @@
 #!/bin/bash
-
-# Function to prepend datetime to log entries
 log_entry() {
     local timestamp=$(date "+%Y-%m-%d %H:%M:%S")
     echo "$timestamp $1" >> "$LOG_FILE"
 }
 
-# Change to the algo directory
+# Function to check internet connectivity
+check_internet() {
+    # Ping Google's DNS server
+    ping -c 1 8.8.8.8 &>/dev/null
+    if [ $? -eq 0 ]; then
+        return 0  # Internet connection exists
+    else
+        return 1  # No internet connection
+    fi
+}
+
+
 cd algo
-# Set up logging
 LOG_FILE="algo.log"
 exec > >(tee -a "$LOG_FILE") 2>&1
-
-# Git pull
-if ! git pull; then
-    log_entry "Git pull failed. Attempting to stash and retry."
-    git stash
-    if ! git pull; then
-        log_entry "Second git pull failed. Exiting."
-        exit 1
+if check_internet; then
+    git pull
+    if [$? -ne 0 ]; then
+        log_entry "Git pull success."
+        npm install
+        if [$? -ne 0 ]; then
+            log_entry "npm install success!"
+            npm run start
+        else 
+            log_entry "npm install failed!"
+            npm run start
+        fi
+    else
+        log_entry "Git pull not success."
+        npm run start
     fi
-fi
-
-# Check if any changes were pulled
-if git status --porcelain | grep -q '^M'; then
-    log_entry "Changes detected after git pull. Installing dependencies..."
-    npm install
 else
-    log_entry "No changes detected after git pull. Skipping npm install."
+    log_entry "No internet, defaulting to start.+"
+   npm run start
 fi
 
-log_entry "Starting application..."
-npm run start
+# # Function to prepend datetime to log entries
+# log_entry() {
+#     local timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+#     echo "$timestamp $1" >> "$LOG_FILE"
+# }
+
+# # Change to the algo directory
+# cd algo
+# # Set up logging
+# LOG_FILE="algo.log"
+# exec > >(tee -a "$LOG_FILE") 2>&1
+
+# # Git pull
+# if ! git pull; then
+#     log_entry "Git pull failed. Attempting to stash and retry."
+#     git stash
+#     if ! git pull; then
+#         log_entry "Second git pull failed. Exiting."
+#         exit 1
+#     fi
+# fi
+
+# # Check if any changes were pulled
+# if git status --porcelain | grep -q '^M'; then
+#     log_entry "Changes detected after git pull. Installing dependencies..."
+#     npm install
+# else
+#     log_entry "No changes detected after git pull. Skipping npm install."
+# fi
+
+# log_entry "Starting application..."
+# npm run start
