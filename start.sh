33967765a9fc1f5c127b7a6d20d8bf186ff71255
Gabe Ulrich Laptop <gabriel.ulrich@aol.com>
2024-12-16 10:00:02 -0500

    sh

--- start.sh
+++ start.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
-
-check_internet() {
-    # Ping Google's DNS server
-    ping -c 1 8.8.8.8 &>/dev/null
-    if [ $? -eq 0 ]; then
-        return 0  # Internet connection exists
-    else
-        return 1  # No internet connection
-    fi
-}
-
-
-if check_internet; then
-    git pull
-    npm install
-    npm run start
-else
-    npm run start
-fi
+npm run start
